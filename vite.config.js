6f70e61a23b780ac2899e9a2a0d16d2f8254b994
Kolawole Akintayo <kolawoleakintayok@gmail.com>
2026-08-11 20:01:57 +0100

    Update vite config to be able to build successfully

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,23 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        }
-      }
-    }
-  }
-})
+        manualChunks(id) {
+          if (
+            id.includes("node_modules/react") ||
+            id.includes("node_modules/react-dom") ||
+            id.includes("node_modules/react-router-dom")
+          ) {
+            return "vendor";
+          }
+
+          if (
+            id.includes("node_modules/qrcode") ||
+            id.includes("node_modules/html2canvas")
+          ) {
+            return "media";
+          }
+        },
+      },
+    },
+  },
+});
